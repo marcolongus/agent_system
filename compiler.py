1a9244bda3f58cf1fce33f3ff76920fc516cec23
marcolongus <benjaminmarcolongo@gmail.com>
2021-07-12 16:20:23 -0300

    makefile trucho

--- compiler.py
+++ compiler.py
@@ -1,2 +1,27 @@
 import os
-os.system('cmd /k "gcc -o agentes agentes.cpp -lstdc++ -O3 -march=native -fopenmp"')
+from sys import argv
+
+#compilacion = "gcc -o agentes_test agentes.cpp -lstdc++ -O3 -march=native -fopenmp"
+compiler   = "gcc -o "
+target     = "agentes_test "
+program    = "agentes.cpp "
+flags      = "-lstdc++ -pedantic-errors " # -Wall " #-Werrors
+opt_flags  = "-O3 -march=native -fopenmp "
+
+compilacion =compiler + target + program + flags + opt_flags
+print(compilacion)
+
+flush = "del "+"agentes_test.exe"
+
+os.system("%s" %compilacion)
+try:
+	flusher = int(argv[1])
+	if flusher:
+		print("Flush")
+		os.system("%s" %flush)
+except:
+	pass
+
+
+
+
