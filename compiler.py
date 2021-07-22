96ef70e00e1336d2251b7624d58560203810533d
marcolongus <benjaminmarcolongo@gmail.com>
2021-07-22 17:33:26 -0300

    error arreglado

--- compiler.py
+++ compiler.py
@@ -2,7 +2,7 @@ import os
 from sys import argv
 from termcolor import colored,cprint
 import os
-os.system('color')
+#os.system('color'), no hace falta en linux
 
 #compilacion = "gcc -o agentes_test agentes.cpp -lstdc++ -O3 -march=native -fopenmp"
 compiler   = "gcc -o "
